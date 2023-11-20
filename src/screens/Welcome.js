f933db4bc53ab8d83a0c2d2c9539701a645e968a
k2-ux <maxkamalarea@gmail.com>
2023-11-20 15:12:32 +0530

    app icon

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,36 +1,51 @@
 import React from 'react';
-import { View, FlatList, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-const{height,width} = Dimensions.get('screen' )
+import {
+  View,
+  FlatList,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+const {height, width} = Dimensions.get('screen');
+import {SvgXml} from 'react-native-svg';
+import {xml} from '../components/svg.js';
 const Welcome = () => {
   const navigation = useNavigation();
 
   const screens = [
-    { name: 'Carousel', component: 'FlatListCarousel' },
-    { name: 'Layout', component: 'WpAnimation' },
-    { name: 'Switch', component: 'AnimatedSwitch' },
-    { name: 'Slider', component: 'AnimatedSliderDemo' },
-    { name: 'Swipe', component: 'DemoSwipeAnimation' },
-    { name: 'Search', component: 'SearchBox' },
-    { name: 'Instagram', component: 'InstagramAnimation' },
-    { name: 'Scroll', component: 'ScrollAnimation' },
+    {name: 'Carousel', component: 'Anime Carousel'},
+    {name: 'Switch', component: 'Change theme'},
+    {name: 'Slider', component: 'Superhero Slides'},
+    {name: 'Swipe', component: 'Swipe demo'},
+    {name: 'Search', component: 'SearchBox demo'},
+    {name: 'Instagram', component: 'Heart react demo'},
+    {name: 'Scroll', component: 'Drag and drop'},
+    {name: 'Layout', component: 'About me'},
   ];
 
-  const renderItem = ({ item }) => (
+  const renderItem = ({item}) => (
     <TouchableOpacity
       style={styles.itemContainer}
-      onPress={() => navigation.navigate(item.name)}
-    >
-      <Text style={styles.itemText}>{item.name}</Text>
+      onPress={() => navigation.navigate(item.name)}>
+      <Text style={styles.itemText}>{item.component}</Text>
     </TouchableOpacity>
   );
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+        <SvgXml xml={xml} width={50} height={50} style={{borderRadius: 10}} />
+        <Text style={styles.headerText}>
+          Welcome! Take a look at the items below
+        </Text>
+      </View>
       <FlatList
         data={screens}
+        showsVerticalScrollIndicator={false}
         renderItem={renderItem}
-        keyExtractor={(item) => item.name}
+        keyExtractor={item => item.name}
       />
     </View>
   );
@@ -41,22 +56,38 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 16,
-    // backgroundColor:'yellow'
+    // padding: 16,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    height: height * 0.1,
+    backgroundColor: '#71B48D',
+    elevation: 30,
+    paddingHorizontal: 5,
+  },
+  headerText: {
+    marginLeft: 10,
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
   },
   itemContainer: {
-    flex:1,
+    flex: 1,
     padding: 16,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
-    width: width*0.9,
+    width: width * 0.9,
     alignItems: 'center',
-    backgroundColor:'#DBEFBC',
-    marginBottom:5,
-    height:height/8,
-    borderRadius:10,
-    // elevation:10,
-    justifyContent:'center'
+    backgroundColor: '#DBEFBC',
+    marginBottom: 5,
+    height: height / 8,
+    borderRadius: 10,
+    justifyContent: 'center',
   },
   itemText: {
     fontSize: 18,
